6d265e4dad0d5789945737bb804da864d0e5a7af
Turbobot <turbobot@vercel.com>
2025-06-04 21:32:00 +0530

    feat(create-turbo): apply official-starter transform

--- apps/backend/src/server.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import express from "express";
-import { PORT } from "./constants";
-
-const app = express();
-
-app.listen(PORT, () => {
-  console.log("server is listening on port: ", PORT);
-});
--- apps/web/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@repo/ui/components/button';
-
-export default function App() {
-  return (
-    <div>
-      <Button variant="destructive">Click me</Button>
-    </div>
-  );
-}
